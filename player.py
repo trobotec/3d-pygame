a919f4b404d42c64501664b03524f2003d34f804
trobotec <timofei.robotec@gmail.com>
2022-02-13 14:23:24 +0700

    First commit

---
+++ player.py
@@ -0,0 +1,33 @@
+from select import select
+from settings import *
+import pygame
+
+class Player:
+    def __init__(self):
+        self.x, self.y = player_pos
+        self.angle = player_angle
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    def movement(self):
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_w]:
+            self.y -= player_speed
+
+        if key[pygame.K_s]:
+            self.y += player_speed
+
+        if key[pygame.K_a]:
+            self.x -= player_speed
+
+        if key[pygame.K_d]:
+            self.x += player_speed
+
+        if key[pygame.K_LEFT]:
+            self.angle -= 0.02
+
+        if key[pygame.K_RIGHT]:
+            self.angle += 0.02
---
+++ settings.py
@@ -0,0 +1,19 @@
+# game settings
+WIDTH = 1000
+HEIGHT = 600
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+
+# playr settings
+player_pos = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+player_speed =  2
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 220, 0)
+BLUE = (0, 0, 220)
+DARKGRAY = (110, 110, 110)
+PURPL = (120, 0, 120)
